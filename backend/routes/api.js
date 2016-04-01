599943b55f6ef6d6cb3f64875e2bd26fe6362987
Ning, Tao <ningtaous@gmail.com>
2016-04-01 16:48:12 -0400

    feat: add mongodb both locally and on heroku as add on

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,12 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/api/v1', function(req, res, next) {
-  res.send('get request for /api/v1');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+var uristring =
+  process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost:27017/daniel';
+
+MongoClient.connect(uristring, function(err, db) {
+
+  assert.equal(err, null);
+  console.log("Successfully connected to MongoDB.");
+
+  router.get('/api/v1', function(req, res) {
+    res.send('get request for /api/v1');
+  });
+
+  router.post('/api/v1', function(req, res) {
+    res.send('post request for /api/v1');
+  });
+
+
 });
 
-router.post('/api/v1', function(req, res, next) {
-  res.send('post request for /api/v1');
-});
 
 module.exports = router;
