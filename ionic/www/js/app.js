7b4394dd691eebd57433f956264387446cdcd6af
Ning, Tao <ningtaous@gmail.com>
2016-04-08 14:55:29 -0400

    style: modify code style according to eslint

--- ionic/www/js/app.js
+++ ionic/www/js/app.js
@@ -5,71 +5,67 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic','ionic.service.core', 'starter.controllers', 'starter.services'])
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.recording', {
-    url: '/recording',
-    views: {
-      'tab-recording': {
-        templateUrl: 'templates/tab-recording.html',
-        controller: 'RecordingCtrl as rctrl'
+angular.module('starter', ['ionic', 'ionic.service.core', 'starter.controllers', 'starter.services'])
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
       }
-    }
-  })
-
-  .state('tab.history', {
-      url: '/history',
-      views: {
-        'tab-history': {
-          templateUrl: 'templates/tab-history.html',
-          controller: 'HistoryCtrl as hctrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/history/:chatId',
-      views: {
-        'tab-history': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'HistoryDetailCtrl'
-        }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
       }
     });
+  })
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/recording');
+  .config(function ($stateProvider, $urlRouterProvider) {
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
 
-});
+    // setup an abstract state for the tabs directive
+      .state('tab', {
+        url: '/tab',
+        abstract: true,
+        templateUrl: 'templates/tabs.html'
+      })
+
+      // Each tab has its own nav history stack:
+
+      .state('tab.recording', {
+        url: '/recording',
+        views: {
+          'tab-recording': {
+            templateUrl: 'templates/tab-recording.html',
+            controller: 'RecordingCtrl as rctrl'
+          }
+        }
+      })
+
+      .state('tab.history', {
+        url: '/history',
+        views: {
+          'tab-history': {
+            templateUrl: 'templates/tab-history.html',
+            controller: 'HistoryCtrl as hctrl'
+          }
+        }
+      })
+      .state('tab.chat-detail', {
+        url: '/history/:chatId',
+        views: {
+          'tab-history': {
+            templateUrl: 'templates/chat-detail.html',
+            controller: 'HistoryDetailCtrl'
+          }
+        }
+      });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/recording');
+  });
--- ionic/www/js/controllers.js
+++ ionic/www/js/controllers.js
@@ -4,109 +4,110 @@ angular.module('starter.controllers', ['ionic'])
     var self = this;
     // properties to card 1
     self.inFeedingState = false;
-    self.txtBtn1 = "Feeding Daniel";
+    self.txtBtn1 = 'Feeding Daniel';
     self.feedingAmount = 50;
 
     // properties to card 2
     self.inChangingDiapelState = false;
-    self.txtBtn2 = "Changing Daniel's Diaper";
+    self.txtBtn2 = 'Changing Daniel\'s Diaper';
     self.wetPersentage = 50;
 
     self.feeding = function () {
       if (!self.inFeedingState) {
         self.inFeedingState = true;
         self.inChangingDiapelState = false;
-        self.txtBtn1 = "Click again to record";
+        self.txtBtn1 = 'Click again to record';
       } else {
         // set record
-        StorageFactory.setStorage(self.feedingAmount, "feeding");
-        self.txtBtn1 = "Recording to server...";
+        StorageFactory.setStorage(self.feedingAmount, 'feeding');
+        self.txtBtn1 = 'Recording to server...';
         self.disableCard = true;
         $timeout(function () {
-          self.txtBtn1 = "Feeding Daniel";
+          self.txtBtn1 = 'Feeding Daniel';
           self.disableCard = false;
           self.inFeedingState = false;
           self.feedingAmount = 50;
-        }, 1500)
+        }, 1500);
       }
-    }
+    };
 
     self.changing = function () {
       if (!self.inChangingDiapelState) {
         self.inFeedingState = false;
         self.inChangingDiapelState = true;
-        self.txtBtn2 = "Click again to record";
+        self.txtBtn2 = 'Click again to record';
       } else {
         // set record
-        StorageFactory.setStorage(self.wetPersentage, "changing");
-        self.txtBtn2 = "Recording to server...";
+        StorageFactory.setStorage(self.wetPersentage, 'changing');
+        self.txtBtn2 = 'Recording to server...';
         self.disableCard = true;
         $timeout(function () {
-          self.txtBtn2 = "Changing Daniel's Diaper";
+          self.txtBtn2 = 'Changing Daniel\'s Diaper';
           self.disableCard = false;
           self.inChangingDiapelState = false;
           self.wetPersentage = 50;
-        }, 1500)
+        }, 1500);
       }
-    }
+    };
   })
 
-  .controller('HistoryCtrl', function ($scope, $timeout,StorageFactory) {
+  .controller('HistoryCtrl', function ($scope, $timeout, StorageFactory) {
     var self = this;
     self.items = StorageFactory.getStorage();
-    self.filterBy ='';
+    self.filterBy = '';
 
-    self.doRefresh = function() {
+    self.doRefresh = function () {
       console.log(self.filterBy);
-      console.log("doRefresh");
+      console.log('doRefresh');
       self.items = StorageFactory.getStorage();
-      $timeout(function(){
-        console.log("Done, now broadcasting");
+      $timeout(function () {
+        console.log('Done, now broadcasting');
         $scope.$broadcast('scroll.refreshComplete');
-      },1500)
-    }
+      }, 1500);
+    };
   })
 
   .factory('StorageFactory', function ($window) {
     var store = $window.localStorage;
 
-    return {
-      getStorage: getStorage,
-      setStorage: setStorage
-    };
-
     // return an obj contains 2 arrays
     function getStorage() {
-      var feeding = store.getItem("feeding");
-      var changing = store.getItem("changing");
+      var feeding = store.getItem('feeding');
+      var changing = store.getItem('changing');
       return {
         feedingArray: JSON.parse(feeding),
         changingArray: JSON.parse(changing)
-      }
+      };
     }
 
     // push data into one array based on the key
     function setStorage(amountParam, key) {
       // create an object to push in array in storage
       var keyStr;
-      if (key === "feeding") {
-        keyStr = "Dianel got hungry, so we fed him";
-      } else {
-        keyStr = "Oops, he do it again, so we dress him a new diaper";
-      }
+      var array;
       var obj = {
         type: keyStr,
         time: Date.now(),
-        amount:amountParam
+        amount: amountParam
       };
+
+      if (key === 'feeding') {
+        keyStr = 'Dianel got hungry, so we fed him';
+      } else {
+        keyStr = 'Oops, he do it again, so we dress him a new diaper';
+      }
 
       if (!store.getItem(key)) {
         store.setItem(key, JSON.stringify([obj]));
       } else {
-        var array = JSON.parse(store.getItem(key));
+        array = JSON.parse(store.getItem(key));
         array.push(obj);
         store.setItem(key, JSON.stringify(array));
       }
     }
 
+    return {
+      getStorage: getStorage,
+      setStorage: setStorage
+    };
   });
