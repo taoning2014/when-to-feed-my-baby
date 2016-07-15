144c6acf1f5238a5cba42723985ddd3b68545d5f
Tao Ning <ningtaous@gmail.com>
2016-07-15 10:22:28 -0400

    refactor app.js

--- ionic/www/js/app.js
+++ ionic/www/js/app.js
@@ -1,3 +1,4 @@
+(function () {
 // Ionic Starter App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
@@ -5,64 +6,64 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'ionic.service.core', 'starter.controllers', 'starter.services', 'starter.filter'])
-  .run(function($ionicPlatform) {
-    $ionicPlatform.ready(function() {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        cordova.plugins.Keyboard.disableScroll(true);
-      }
-      if (window.StatusBar) {
-        // org.apache.cordova.statusbar required
-        StatusBar.styleDefault();
-      }
-    });
-  })
-
-  .config(function($stateProvider, $urlRouterProvider) {
-    // Ionic uses AngularUI Router which uses the concept of states
-    // Learn more here: https://github.com/angular-ui/ui-router
-    // Set up the various states which the app can be in.
-    // Each state's controller can be found in controllers.js
-    $stateProvider
-
-    // setup an abstract state for the tabs directive
-      .state('tab', {
-        url: '/tab',
-        abstract: true,
-        templateUrl: 'templates/tabs.html'
-      })
-
-      // Each tab has its own nav history stack:
-
-      .state('tab.recording', {
-        url: '/recording',
-        views: {
-          'tab-recording': {
-            templateUrl: 'templates/tab-recording.html',
-            controller: 'RecordingCtrl as rctrl'
-          }
+  angular.module('starter', ['ionic', 'ionic.service.core', 'starter.controllers', 'starter.services',
+      'starter.filter'])
+    .run(function ($ionicPlatform) {
+      $ionicPlatform.ready(function () {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+          cordova.plugins.Keyboard.disableScroll(true);
         }
-      })
-
-      .state('tab.history', {
-        // get data before load this page
-        resolve: {
-          data: function(DataFactory) {
-            return DataFactory.getData(null);
-          }
-        },
-        url: '/history',
-        views: {
-          'tab-history': {
-            templateUrl: 'templates/tab-history.html',
-            controller: 'HistoryCtrl as hctrl'
-          }
+        if (window.StatusBar) {
+          // org.apache.cordova.statusbar required
+          StatusBar.styleDefault();
         }
       });
+    })
 
-    // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/tab/recording');
-  });
+    .config(function ($stateProvider, $urlRouterProvider) {
+      // Ionic uses AngularUI Router which uses the concept of states
+      // Learn more here: https://github.com/angular-ui/ui-router
+      // Set up the various states which the app can be in.
+      // Each state's controller can be found in controllers.js
+      $stateProvider
+
+      // setup an abstract state for the tabs directive
+        .state('tab', {
+          url: '/tab',
+          abstract: true,
+          templateUrl: 'templates/tabs.html'
+        })
+
+        // Each tab has its own nav history stack:
+        .state('tab.recording', {
+          url: '/recording',
+          views: {
+            'tab-recording': {
+              templateUrl: 'templates/tab-recording.html',
+              controller: 'RecordingCtrl as rctrl'
+            }
+          }
+        })
+
+        .state('tab.history', {
+          resolve: {
+            data: function (DataFactory) {
+              return DataFactory.getData();
+            }
+          },
+          url: '/history',
+          views: {
+            'tab-history': {
+              templateUrl: 'templates/tab-history.html',
+              controller: 'HistoryCtrl as hctrl'
+            }
+          }
+        });
+
+      // if none of the above states are matched, use this as the fallback
+      $urlRouterProvider.otherwise('/tab/recording');
+    });
+})();
