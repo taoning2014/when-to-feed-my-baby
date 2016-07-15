af73e04da7b96061dcf999887c463f19f124927e
Tao Ning <ningtaous@gmail.com>
2016-07-15 12:27:01 -0400

    refactor server side code

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -7,29 +7,29 @@ elastic.indexExists().then(function (exists) {
   if (exists) {
     console.log('index exist, don\'t load init data');
     return;
-  } else {
-    elastic
-      .initIndex()
-      .then(elastic.initMapping())
-      .then(function () {
-        console.log('Debug: ');
-        var test = require('../db/initData.js');
-        console.log(test);
-        test.map(function (dataObj) {
-          return elastic.addDocument(dataObj);
-        });
-        console.log('init data load success');
-      });
   }
+
+  console.log('index not exist, init db');
+  elastic
+    .initIndex()
+    .then(elastic.initMapping())
+    .then(function () {
+      var test = require('../db/initData.js');
+      test.map(function (dataObj) {
+        return elastic.addDocument(dataObj);
+      });
+      console.log('init data load success');
+    });
 });
 
 router.get('/api/v1', function (req, res) {
   elastic.searchDocument().then(function (data) {
-    // don't know how to only return _source filed, so process data here
     var processedData = data.hits.hits
+      // don't know how to only return _source filed, so process data here
       .map(function (item) {
         return item._source;
-      }).sort(function(item1, item2) {
+      // don't know how to only return 10 most recent data, so sort and slice here
+      }).sort(function (item1, item2) {
         return -item1.date.localeCompare(item2.date);
       }).slice(0, 10);
 
@@ -38,7 +38,7 @@ router.get('/api/v1', function (req, res) {
 });
 
 router.post('/api/v1', function (req, res) {
-  console.log('Got post request');
+  console.log('Got post request: ');
   console.log(req.body);
   elastic.addDocument(req.body).then(function () {
     res.end('add success!');
